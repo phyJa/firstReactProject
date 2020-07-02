b56f2433c1524dc43c5f05fdaa00ac8a01c833c1
Elias <eli.bueno9@gmail.com>
2020-07-02 12:32:31 -0300

    Adding the states object

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import Todos from './components/Todos';
 
 function App() {
+  //Let's start talking about states
+  let states = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with friends',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  };
   return (
     <div className="App">
       <Todos />
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,5 @@
 //Note we wrote Todos.js with a capital T. This is a convention for naming components
 import React from 'react';
-//import './App.css';
 
 function Todos() {
   return (
