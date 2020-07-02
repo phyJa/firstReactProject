8426d2a344fbe23573e3f859ce1fb72e031810a1
Elias <eli.bueno9@gmail.com>
2020-07-02 10:33:18 -0300

    Index.css removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 // We do not need this yet:
 // import * as serviceWorker from './serviceWorker';
