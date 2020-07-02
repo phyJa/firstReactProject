d26b625ba6b05b59b0a13dac98fbb8a85710b1d8
Elias <eli.bueno9@gmail.com>
2020-07-02 10:46:04 -0300

    Header removed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Starting the app</h1>
     </div>
   );
 }
