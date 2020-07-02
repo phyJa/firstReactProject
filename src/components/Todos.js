206b2542091f37caff0043fe5eaf31374b78a636
Elias <eli.bueno9@gmail.com>
2020-07-02 11:55:03 -0300

    Adding the first component

---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+//Note we wrote Todos.js with a capital T. This is a convention for naming components
+import React from 'react';
+//import './App.css';
+
+function Todos() {
+  return (
+    <div>
+      <h1>Starting the app</h1>
+    </div>
+  );
+}
+
+export default Todos;
