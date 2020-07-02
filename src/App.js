4011f3efd575bb7421a99a1455633f04196f104f
Elias <eli.bueno9@gmail.com>
2020-07-02 12:04:42 -0300

    Displaying the app in another form (using App.js and Todo.js)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
-      <h1>Starting the app</h1>
+      <Todos />
     </div>
   );
 }
