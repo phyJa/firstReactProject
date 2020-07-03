54172ecda9bbd5a8e19140d3f6d544b690c5ea99
Elias <eli.bueno9@gmail.com>
2020-07-03 20:29:37 -0300

    Trying to find a way to pass the properties from Todos.js to App.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,31 @@
 //Note we wrote Todos.js with a capital T. This is a convention for naming components
 import React from 'react';
 
+//Define your states
+let states = {
+  todos: [
+    {
+      id: 1,
+      title: 'Take out the trash',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Dinner with friends',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Meeting with boss',
+      completed: false
+    }
+  ]
+};
+
 function Todos() {
+  //Return your states in html code
   return (
-    <div>
-      <h1>Component</h1>
-    </div>
+    states.todos.map((todo) => (<h3 id={todo.title}>{todo.title}</h3>))
   );
 }
 
