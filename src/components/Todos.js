b4a0549be51f8ffac4b3d4dcc346bbaf81ee91cc
Elias <eli.bueno9@gmail.com>
2020-07-04 10:07:00 -0300

    Getting rid of the key errors

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,15 @@
 //Note we wrote Todos.js with a capital T. This is a convention for naming components
 import React from 'react';
+//Import TodoItem
+import TodoItem from './TodoItem'
 
 class Todos extends React.Component {
   //Return your states in html code
   render() {
     return (
-      this.props.todos.map( (todo) => (<h3>{todo.title}</h3>) )
+      //Pass each todo Object to <TodoItem />
+      //Define the key prop to get rid of the error (be sure that it is unique for each value)
+      this.props.todos.map( (todo) => (<TodoItem key={todo.id} todo={todo} />) )
     );
   }
 }
