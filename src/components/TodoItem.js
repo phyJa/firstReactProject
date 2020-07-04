8b52827e54837b70f2d342585927eb2ab403aeda
Elias <eli.bueno9@gmail.com>
2020-07-04 19:07:25 -0300

    Adding some styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,12 +5,21 @@ import PropTypes from 'prop-types';
 export class TodoItem extends Component {
     render() {
         return (
-            <div>
+            // Inline style with JSX style = {{propertyInCamelCase: 'value'}}
+            // Or using variables style = {varName}
+            <div style = {styling}>
               <p> {this.props.todo.title} </p>  
             </div>
         )
     }
 }
+
+//Variable for color and other styles. Note that it must be an object
+const styling = {
+    backgroundColor: 'tomato',
+    textAlign: 'center',
+    fontSize: 20
+};
 
 //PropType from Todos.js
 TodoItem.propTypes = {
