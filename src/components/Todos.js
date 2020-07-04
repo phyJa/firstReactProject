d46a23b3ab0d59b9d250b14a2843ec051ff1e3d6
Elias <eli.bueno9@gmail.com>
2020-07-04 09:48:13 -0300

    Accessing and displaying data from App.js in Todos.js

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,9 +4,8 @@ import React from 'react';
 class Todos extends React.Component {
   //Return your states in html code
   render() {
-    console.log(this.props.todos);
     return (
-      <h1> Todos </h1>
+      this.props.todos.map( (todo) => (<h3>{todo.title}</h3>) )
     );
   }
 }
