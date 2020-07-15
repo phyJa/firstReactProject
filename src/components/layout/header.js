ff0b8907f384cfce2767a5240ac2a20ff9035175
Elias <eli.bueno9@gmail.com>
2020-07-15 12:33:39 -0300

    Styling header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function Header() {
+    const headerStyle = {
+        backgroundColor: "#555",
+        textAlign: "center",
+        color: "white",
+        padding: "10px"
+    };
     return(
-        <header>
+        <header style = {headerStyle}>
             <h1> TodoList </h1>
         </header>
     );
