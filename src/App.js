89340026c1165400cc704d25282798375f73b015
Elias <eli.bueno9@gmail.com>
2020-07-15 12:25:40 -0300

    Adding a header for the application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import Header from './components/layout/header';
 
 class App extends React.Component {
   //Define your states
@@ -53,6 +54,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
