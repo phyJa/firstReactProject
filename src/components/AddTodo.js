1eb3000841ddcfa346aa891f7de1909119a10e0d
Elias <eli.bueno9@gmail.com>
2020-07-20 11:29:15 -0300

    Another way of changing the state in input: it is more useful when you have to change multiple states

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -11,7 +11,7 @@ export default class AddTodo extends React.Component {
     
     onChange = (e) => {
         this.setState({
-            title: e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
